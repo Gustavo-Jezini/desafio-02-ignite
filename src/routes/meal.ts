345bcf46e33534b7d64fc68d2d682cb5bb354214
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-05-30 08:58:34 -0300

    feat: initalizate create user route

--- src/routes/meal.ts
+++ src/routes/meal.ts
@@ -1,34 +1,34 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance } from "fastify"
 
 export async function mealsRoutes(app: FastifyInstance) {
   
   // Deve ser possivel criar refeições
-  app.post('/', (response, reply) => {
+  app.post('/', async (request, reply) => {
     
   })
   
   // Deve ser possível apagar uma refeição
-  app.delete('/', (response, reply) => {
+  app.delete('/', async (request, reply) => {
   
   })
     
   // Deve ser possível editar uma refeição, podendo alterar todos os dados acima
-  app.put('/', (response, reply) => {
+  app.put('/', async (request, reply) => {
     
   })
   
   // Deve ser possível listar todas as refeições de um usuário
-  app.get('/', (response, reply) => {
+  app.get('/', async (request, reply) => {
   
   })
   
   // Deve ser possível visualizar uma única refeição
-  app.get('/:id', (response, reply) => {
+  app.get('/:id', async (request, reply) => {
 
   })
 
   // Deve ser possível recuperar as métricas de um usuário
-  app.get('/summary', (response, reply) => {
+  app.get('/summary', async (request, reply) => {
   
   })
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,4 +2,13 @@ import { FastifyInstance } from "fastify";
 
 export async function usersRoutes(app: FastifyInstance) {
   
+  // Deve ser possivel criar usuário
+  app.post('/', async (request, reply) => {
+    const { 
+      name,
+      description,
+      created_at,
+      isOnDiet
+     } = request.body
+  })
 }
